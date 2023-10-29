e21c1b98b80f7d2f11725481f78b2390fba7a893
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-10-29 20:33:35 +0000

    implement pagination

---
+++ src/components/ListPagination/ListPagination.tsx
@@ -0,0 +1,36 @@
+import { Pagination } from 'react-bootstrap'
+import './listpagination.css'
+
+interface IListPaginationProps {
+  currentPage: number
+  totalPages: number
+  paginate: (pageNumber: number) => void
+}
+
+export default function ListPagination({
+  currentPage,
+  totalPages,
+  paginate,
+}: Readonly<IListPaginationProps>) {
+  return (
+    <Pagination>
+      <Pagination.First onClick={() => paginate(1)} />
+      <Pagination.Prev
+        onClick={() => {
+          if (currentPage > 1) {
+            paginate(currentPage - 1)
+          }
+        }}
+      />
+      <Pagination.Item active>{currentPage}</Pagination.Item>
+      <Pagination.Next
+        onClick={() => {
+          if (currentPage < totalPages) {
+            paginate(currentPage + 1)
+          }
+        }}
+      />
+      <Pagination.Last onClick={() => paginate(totalPages)} />
+    </Pagination>
+  )
+}
--- src/pages/PetSearch/PetSearch.tsx
+++ src/pages/PetSearch/PetSearch.tsx
@@ -1,18 +1,39 @@
+import { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import CatCard from '../../components/CatCard/CatCard'
+import ListPagination from '../../components/ListPagination/ListPagination'
 import useCatList from '../../hooks/useCatList'
 
 export default function PetSearch() {
   const cats = useCatList()
+
+  //handle pagination
+  const itemsPerPage = 5
+  const [currentPage, setCurrentPage] = useState(1)
+  const indexOfLastCat = currentPage * itemsPerPage
+  const indexOfFirstCat = indexOfLastCat - itemsPerPage
+  const currentCats = cats.slice(indexOfFirstCat, indexOfLastCat)
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+  const totalPages = Math.ceil(cats.length / itemsPerPage)
+
   return (
     <Container>
-      {cats.map((item) => (
-        <Row className="my-3">
+      {currentCats.map((item) => (
+        <Row className="my-3" key={item.id}>
           <Col xs={{ span: 10, offset: 1 }}>
-            <CatCard cat={item} key={item.id} />
+            <CatCard cat={item} />
           </Col>
         </Row>
       ))}
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <ListPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            paginate={paginate}
+          />
+        </Col>
+      </Row>
     </Container>
   )
 }
